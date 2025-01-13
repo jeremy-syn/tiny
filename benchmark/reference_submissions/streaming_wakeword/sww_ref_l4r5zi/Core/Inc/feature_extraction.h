95446419c856ccc1157370176bab51c2ca235692
Jeremy Holleman <jeremy@syntiant.com>
2025-01-13 10:25:03 -0500

    added sww_ref_l4r5zi, reference implementation for sww.  currently really just a holding spot

---
+++ benchmark/reference_submissions/streaming_wakeword/sww_ref_l4r5zi/Core/Inc/feature_extraction.h
@@ -0,0 +1,13 @@
+/*
+ * feature_extraction.h
+ *
+ *  Created on: Jan 12, 2025
+ *      Author: jeremy
+ */
+
+#ifndef INC_FEATURE_EXTRACTION_H_
+#define INC_FEATURE_EXTRACTION_H_
+
+void test_extraction(void);
+
+#endif /* INC_FEATURE_EXTRACTION_H_ */
